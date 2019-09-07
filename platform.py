7ec2575f41be1aae93bd9398ba3000ed9a2123ed
qitas <qitas@qitas.cn>
2019-09-07 22:32:02 +0800

    fix

--- platform.py
+++ platform.py
@@ -1,6 +1,6 @@
 from platformio.managers.platform import PlatformBase
 
-class ESP12Platform(PlatformBase):
+class Esp12Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         framework = variables.get("pioframework")
